96f91a0279f3990c936f9902346c6f09c5fe17ab
sohunjug <sohunjug@hotmail.com>
2022-03-13 00:54:23 +0800

    update

--- flake.nix
+++ flake.nix
@@ -5,17 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nurpkgs = {
-      url = github:nix-community/NUR;
+      url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nurpkgs, ... }@inputs: {
 
     nixosConfigurations."sohunjug-ls" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
